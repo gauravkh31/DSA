c1f9bda11f27a7d781a39b5ef4051d474169a80a
gauravkh31 <khandelwalgaurav3100@gmail.com>
2024-10-16 02:53:56 +0530

    crt9

---
+++ crt9.java
@@ -0,0 +1,60 @@
+// import java.util.*;
+// public class crt9 {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter value of n: ");
+//         int n = sc.nextInt();
+//         print(n);
+//     }
+//      static void print(int n){
+//         if(n==0){
+            
+//             return;
+//         }
+//         System.out.print(n+ " ");
+//         print(n-1);
+//         System.out.println();
+//         System.out.print(n+ " ");        
+    
+// }}
+
+
+// import java.util.*;
+// public class crt9 {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter value of n: ");
+//         int n = sc.nextInt();
+//         print(n);
+//     }
+//      static void print(int n){
+//         if(n==0){
+            
+//             return;
+//         }
+//         System.out.print(n+ " ");
+//         print(n-1);
+//         System.out.println();
+//         System.out.print(n+ " ");        
+    
+// }}
+
+import java.util.*;
+public class crt9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter value of n: ");
+        int n = sc.nextInt();
+        print(n);
+    }
+     static void print(int n){
+        if(n==0){
+            
+            return;
+        }
+        System.out.print(n+ " ");
+        print(n-1);
+        System.out.println();
+        System.out.print(n+ " ");        
+    
+}}
