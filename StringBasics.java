146f47f585703dfd022e463d33396be75fb5b973
gauravkh31 <khandelwalgaurav3100@gmail.com>
2024-11-11 13:07:46 +0530

    done

--- StringBasics.java
+++ StringBasics.java
@@ -1,8 +1,15 @@
 import java.util.*;
 public class StringBasics {
     public static void main(String[] args) {
-        String s = "gaurav khandelwal";
-        System.out.println(s.contains("gaura kh"));
+        String s = "abcd";
+        // System.out.println(s.contains("gaura kh"));
+
+        for(int i =0; i < s.length(); i++){
+            for(int j = i+1; j < s.length()+1; j++){
+                System.out.print(s.substring(i, j));
+            }
+            System.out.println();
+        }
     }
     
 }
